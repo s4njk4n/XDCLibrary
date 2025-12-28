856b1c660d90ab07b4ea2e71b61c150876fa17a8
s4njk4n <105919271+s4njk4n@users.noreply.github.com>
2025-12-28 20:51:25 +1100

    Update install_xdc_node_v1.0.sh

--- install_xdc_node_v1.0.sh
+++ install_xdc_node_v1.0.sh
@@ -58,8 +58,41 @@ function configureXinFinNode(){
     echo "Clone Xinfin Node"
     git clone https://github.com/XinFinOrg/XinFin-Node && cd XinFin-Node/$Network
     
-    # Automatically remove obsolete 'version' line from docker-compose.yml to avoid warnings
+    # Automatically remove obsolete 'version' line from docker-compose.yml and docker-compose-hash-rpc.yml to avoid warnings
     sed -i '/^version:/d' docker-compose.yml
+    sed -i '/^version:/d' docker-compose-hash-rpc.yml
+    
+    # Fix xdc-attach.sh to use dynamic docker compose exec
+    echo '#!/bin/bash' > xdc-attach.sh
+    echo 'docker compose exec xinfinnetwork XDC attach /work/xdcchain/XDC.ipc' >> xdc-attach.sh
+    chmod +x xdc-attach.sh
+    
+    # Fix peer.sh to use dynamic docker compose exec and correct quoting
+    echo '#!/bin/bash' > peer.sh
+    echo 'filename="enode.txt"' >> peer.sh
+    echo 'while IFS= read -r line || [[ -n "$line" ]]; do' >> peer.sh
+    echo '  echo $line' >> peer.sh
+    echo '  cmd="admin.addPeer(\"$line\")"' >> peer.sh
+    echo '  resp=$(docker compose exec xinfinnetwork XDC --exec "$cmd" attach /work/xdcchain/XDC.ipc)' >> peer.sh
+    echo '  echo $resp' >> peer.sh
+    echo 'done < "$filename"' >> peer.sh
+    chmod +x peer.sh
+    
+    # Fix upgrade.sh to use docker compose, correct nodekey filename, and keep sudo
+    echo '#!/bin/bash' > upgrade.sh
+    echo 'echo "Upgrading XDC Network Configuration Scripts"' >> upgrade.sh
+    echo 'mv .env .env.bak' >> upgrade.sh
+    echo 'mv nodekey nodekey.bak' >> upgrade.sh
+    echo 'git stash' >> upgrade.sh
+    echo 'git pull' >> upgrade.sh
+    echo 'mv .env.bak .env' >> upgrade.sh
+    echo 'mv nodekey.bak nodekey' >> upgrade.sh
+    echo 'echo "Upgrading Docker Images"' >> upgrade.sh
+    echo 'sudo docker pull xinfinorg/xdposchain:v2.4.2-hotfix' >> upgrade.sh
+    echo 'sudo docker compose -f docker-compose.yml down' >> upgrade.sh
+    echo 'git pull' >> upgrade.sh
+    echo 'sudo docker compose -f docker-compose.yml up -d' >> upgrade.sh
+    chmod +x upgrade.sh
     
     echo "Generating Private Key and Wallet Address into keys.json"
     docker build -t address-creator ../address-creator/ && docker run -e NUMBER_OF_KEYS=1 -e FILE=true -v "$(pwd):/work/output" -it address-creator 
@@ -71,6 +104,8 @@ function configureXinFinNode(){
     echo ""
     echo "Starting Xinfin Node ..."
     sudo docker compose -f docker-compose.yml up -d --build --force-recreate
+    echo ""
+    echo ""
 }
 
 function main(){
